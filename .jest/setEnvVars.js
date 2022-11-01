9ebb13c66110b8fd03ab0445354fff818ff27d42
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-01 14:52:34 +0100

    chore: add project setup

---
+++ .jest/setEnvVars.js
@@ -0,0 +1,7 @@
+process.env.BOT_NAME = 'Test';
+process.env.BOT_ACTIVITY = 'Activity';
+process.env.BOT_PREFIX = '!';
+process.env.BOT_TOKEN = 'TOKEN';
+process.env.BOT_MUSIC_CHANNEL = 'MUSIC_CHANNEL';
+process.env.BOT_WELCOME_CHANNEL = 'WELCOME_CHANNEL';
+process.env.BOT_URL = 'discord-bot-player.com';
