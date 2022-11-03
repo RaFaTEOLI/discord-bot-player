cb901632e752d226ee85842baf5ab92e7b89a0b0
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 13:56:13 +0100

    feat: ensure DiscordExecuteCommand will send invalid command message if LoadCommandById returns null

--- src/data/usecases/execute-command/discord-execute-command.ts
+++ src/data/usecases/execute-command/discord-execute-command.ts
@@ -1,11 +1,21 @@
 import { ExecuteCommand } from '@/domain/usecases/execute-command';
 import { LoadCommandById } from '@/domain/usecases/load-command-by-id';
+import { SendMessage } from '@/domain/usecases/send-message';
 
 export class DiscordExecuteCommand implements ExecuteCommand {
-  constructor(private readonly remoteLoadCommandById: LoadCommandById) {}
+  constructor(
+    private readonly remoteLoadCommandById: LoadCommandById,
+    private readonly sendMessageChannel: SendMessage
+  ) {}
 
   async execute(commandId: string): Promise<void> {
-    await this.remoteLoadCommandById.loadById(commandId);
+    const command = await this.remoteLoadCommandById.loadById(commandId);
+    if (!command) {
+      await this.sendMessageChannel.send({
+        title: 'Invalid Command!',
+        description: 'The command you tried is invalid!'
+      });
+    }
     return null;
   }
 }
