cc850ac7b6f899395a261bbd4f36d4a7df0f322e
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-03 14:12:03 +0100

    refactor: update types for CommandModel

--- src/domain/test/mock-command.ts
+++ src/domain/test/mock-command.ts
@@ -3,7 +3,7 @@ import { CommandModel } from '@/domain/models/command';
 export const mockCommand = (): CommandModel => ({
   id: 'any_id',
   command: 'any_command',
-  dispatcher: 'any_dispatcher',
+  dispatcher: 'message',
   type: 'message',
   description: 'any_description',
   response: 'any_response',
