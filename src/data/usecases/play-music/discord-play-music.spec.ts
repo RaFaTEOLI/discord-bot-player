a0e5bc00b81acea3ba498831b4798c05ce9ccd07
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-04 11:28:52 +0100

    test: ensure DiscordPlayMusic will throw exception if queue throws exception

--- src/data/usecases/play-music/discord-play-music.spec.ts
+++ src/data/usecases/play-music/discord-play-music.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { faker } from '@faker-js/faker';
 import { DiscordPlayMusic } from './discord-play-music';
 import { mockDiscordMessage, mockDiscordQueue } from '@/data/test';
@@ -43,5 +44,12 @@ describe('DiscordPlayMusic', () => {
     const song = await sut.play(url);
     expect(song).toBeTruthy();
     expect(song.url).toBe(url);
+  });
+
+  test('should throw exception if queue throws exception', () => {
+    const { sut, discordQueue } = makeSut();
+    jest.spyOn(discordQueue, 'join').mockRejectedValueOnce(new Error());
+    const promise = sut.play(faker.internet.url());
+    expect(promise).rejects.toThrow();
   });
 });
