81e888040df86e8558babe2292da424bcfacd6ec
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-04 11:13:10 +0100

    feat: ensure DiscordPlayMusic will call queue.join with voice channel

--- src/data/usecases/play-music/discord-play-music.ts
+++ src/data/usecases/play-music/discord-play-music.ts
@@ -1,18 +1,16 @@
 import { Message } from 'discord.js';
 import 'dotenv/config';
 import { PlayMusic } from '@/domain/usecases/play-music';
-import { Song } from 'discord-music-player';
-import { DiscordClient } from '@/domain/models/discord-client';
+import { Queue, Song } from 'discord-music-player';
 
 export class DiscordPlayMusic implements PlayMusic {
-  constructor(private readonly client: DiscordClient, private readonly message: Message) {}
+  constructor(private readonly discordQueue: Queue, private readonly message: Message) {}
 
   async play(url: string): Promise<Song> {
     try {
-      this.client.player.createQueue(this.message.guild.id);
+      await this.discordQueue.join(this.message.member.voice.channel);
       return null;
     } catch (error) {
-      console.log(error);
       throw new Error('Error while playing music message');
     }
   }
