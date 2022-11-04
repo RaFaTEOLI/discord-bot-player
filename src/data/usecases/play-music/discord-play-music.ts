1f02e3cebdd73910e0b6459c54fba1c69986ae42
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-04 11:44:23 +0100

    feat: ensure DiscordPlayMusic will return a playlist on queue.playlist success if playlist param is true

--- src/data/usecases/play-music/discord-play-music.ts
+++ src/data/usecases/play-music/discord-play-music.ts
@@ -1,15 +1,15 @@
 import { Message } from 'discord.js';
 import 'dotenv/config';
 import { PlayMusic } from '@/domain/usecases/play-music';
-import { Queue, Song } from 'discord-music-player';
+import { Playlist, Queue, Song } from 'discord-music-player';
 
 export class DiscordPlayMusic implements PlayMusic {
   constructor(private readonly discordQueue: Queue, private readonly message: Message) {}
 
-  async play(url: string, playlist?: boolean): Promise<Song> {
+  async play(url: string, playlist?: boolean): Promise<Song | Playlist> {
     await this.discordQueue.join(this.message.member.voice.channel);
     if (playlist) {
-      await this.discordQueue.playlist(url);
+      return await this.discordQueue.playlist(url);
     }
     return await this.discordQueue.play(url);
   }
--- src/domain/usecases/play-music.ts
+++ src/domain/usecases/play-music.ts
@@ -1,5 +1,5 @@
-import { Song } from 'discord-music-player';
+import { Playlist, Song } from 'discord-music-player';
 
 export interface PlayMusic {
-  play: (url: string, playlist?: boolean) => Promise<Song>;
+  play: (url: string, playlist?: boolean) => Promise<Song | Playlist>;
 }
