a638e1ecc208096053b0466fbe8667c7beb122f5
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2022-11-05 12:56:55 +0100

    feat: ensure RemoteLoadCommands will load commands from API

---
+++ src/data/usecases/load-commands/remote-load-commands.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
+import { CommandModel } from '@/domain/models/command';
+import { LoadCommands } from '@/domain/usecases/load-commands';
+
+export class RemoteLoadCommands implements LoadCommands {
+  constructor(private readonly url: string, private readonly httpGetClient: HttpClient<CommandModel[]>) {}
+
+  async load(): Promise<CommandModel[]> {
+    const httpResponse = await this.httpGetClient.request({
+      url: this.url,
+      method: 'get'
+    });
+    const remoteCommand = httpResponse.body;
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.success:
+        return remoteCommand;
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError();
+      default:
+        throw new UnexpectedError();
+    }
+  }
+}
