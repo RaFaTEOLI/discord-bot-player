95057ddf0f74caabac661e792e393a92a70afeba
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-04-24 13:13:21 -0300

    test: add mock builder to index export

--- src/data/test/index.ts
+++ src/data/test/index.ts
@@ -2,4 +2,6 @@ export * from './mock-embed-builder';
 export * from './mock-send-message-channel';
 export * from './mock-remote-load-command';
 export * from './mock-http';
+export * from './mock-button-builder';
+export * from './mock-action-row-builder';
 export * from './discord';
