8d2ab56fd10e1d7efe9e3ccf5d0f20138b9ed2bb
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-11 12:09:21 -0300

    fix: adjust typo for env vars

--- src/infra/queue/amqp-client.ts
+++ src/infra/queue/amqp-client.ts
@@ -7,7 +7,7 @@ import 'dotenv/config';
 export class AmqpClient<R = SaveMusicParams | SaveQueueParams> implements QueueClient<R> {
   async send(queue: 'music' | 'queue', data: R): Promise<void> {
     const connection = await connect(
-      `amqp://${process.env.AMQP_USERNAME}:${process.env.AMQP_PASSWORD}@${process.env.AMQP_ADRESS}:${process.env.AMQP_PORT}`
+      `amqp://${process.env.BOT_API_QUEUE_USERNAME}:${process.env.BOT_API_QUEUE_PASSWORD}@${process.env.BOT_API_QUEUE_ADRESS}:${process.env.BOT_API_QUEUE_PORT}`
     );
 
     const channel = await connection.createChannel();
