0920d0b340f870109cc444016f2232237e14ede1
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-18 10:11:03 -0300

    test: add shuffle player button

--- src/presentation/helpers/player-buttons/player-buttons.spec.ts
+++ src/presentation/helpers/player-buttons/player-buttons.spec.ts
@@ -2,24 +2,31 @@ import { ButtonStyle } from 'discord.js';
 import { playerButtons } from './player-buttons';
 
 describe('PlayerButtons', () => {
-  test('should have stop emoji on Stop action', () => {
-    expect(playerButtons[0].customId).toBe('stop');
-    expect(playerButtons[0].emoji).toBe('⏹️');
-    expect(playerButtons[0].label).toBe('Stop');
+  test('should have stop emoji on Shuffle action', () => {
+    expect(playerButtons[0].customId).toBe('shuffle');
+    expect(playerButtons[0].emoji).toBe('🔀');
+    expect(playerButtons[0].label).toBe('Shuffle');
     expect(playerButtons[0].style).toBe(ButtonStyle.Secondary);
   });
 
-  test('should have pause emoji on Pause action', () => {
-    expect(playerButtons[1].customId).toBe('pause');
-    expect(playerButtons[1].emoji).toBe('⏯️');
-    expect(playerButtons[1].label).toBe('Pause');
+  test('should have stop emoji on Stop action', () => {
+    expect(playerButtons[1].customId).toBe('stop');
+    expect(playerButtons[1].emoji).toBe('⏹️');
+    expect(playerButtons[1].label).toBe('Stop');
     expect(playerButtons[1].style).toBe(ButtonStyle.Secondary);
   });
 
-  test('should have next emoji on Skip action', () => {
-    expect(playerButtons[2].customId).toBe('skip');
-    expect(playerButtons[2].emoji).toBe('⏭️');
-    expect(playerButtons[2].label).toBe('Skip');
+  test('should have pause emoji on Pause action', () => {
+    expect(playerButtons[2].customId).toBe('togglePlay');
+    expect(playerButtons[2].emoji).toBe('⏯️');
+    expect(playerButtons[2].label).toBe('Pause/Play');
     expect(playerButtons[2].style).toBe(ButtonStyle.Secondary);
+  });
+
+  test('should have next emoji on Skip action', () => {
+    expect(playerButtons[3].customId).toBe('skip');
+    expect(playerButtons[3].emoji).toBe('⏭️');
+    expect(playerButtons[3].label).toBe('Skip');
+    expect(playerButtons[3].style).toBe(ButtonStyle.Secondary);
   });
 });
