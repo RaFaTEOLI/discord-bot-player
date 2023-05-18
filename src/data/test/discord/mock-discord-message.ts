d4ffd2a9276983d899abc767f788b52634890417
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2023-05-18 09:54:48 -0300

    test: ensure the bot will auto-assign itself to a voice channel with members

--- src/data/test/discord/mock-discord-message.ts
+++ src/data/test/discord/mock-discord-message.ts
@@ -1,10 +1,47 @@
 import { faker } from '@faker-js/faker';
+import { Collection, Snowflake, GuildMember } from 'discord.js';
 
 export const mockDiscordMessage = (): any => {
+  const iterable: Iterable<readonly [Snowflake, GuildMember]> = {
+    [faker.datatype.uuid()]: {
+      id: faker.datatype.uuid(),
+      name: faker.internet.userName()
+    },
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return {
+            done: true,
+            value: this
+          };
+        }
+      };
+    }
+  };
+  class DiscordMessageChannel {
+    cache: Array<{
+      type: number;
+      id: string;
+      members: Collection<Snowflake, GuildMember>;
+    }>;
+
+    constructor() {
+      this.cache = [
+        {
+          type: 2,
+          id: faker.datatype.uuid(),
+          members: new Collection(iterable)
+        }
+      ];
+    }
+  }
+
   class DiscordMessageGuild {
     id: string;
+    channels: DiscordMessageChannel;
     constructor() {
       this.id = faker.datatype.uuid();
+      this.channels = new DiscordMessageChannel();
     }
   }
 
