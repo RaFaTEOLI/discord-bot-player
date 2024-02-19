b584984152bc44cbc7e31a798658fb2f673c2f06
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-02-19 18:43:40 -0300

    test: ensure getCommand will return args and command for ChatInputCommandInteraction

--- src/presentation/helpers/discord-commands/discord-commands.spec.ts
+++ src/presentation/helpers/discord-commands/discord-commands.spec.ts
@@ -1,13 +1,39 @@
 import { describe, expect, test } from 'vitest';
 import { getCommand } from './discord-commands';
-import { mockDiscordMessage } from '@/domain/test';
+import { mockDiscordChatInputInteraction, mockDiscordMessage } from '@/domain/test';
+import { faker } from '@faker-js/faker';
 
 describe('DiscordCommands', () => {
-  test('should return a message with args and command', () => {
-    const mockMessage = mockDiscordMessage();
+  describe('Message', () => {
+    test('should return a message with args and command', () => {
+      const mockMessage = mockDiscordMessage();
 
-    mockMessage.content = `${process.env.BOT_PREFIX}playlist link`;
-    const message = getCommand(mockMessage);
-    expect(message).toEqual({ args: 'link', command: 'playlist' });
+      mockMessage.content = `${process.env.BOT_PREFIX}playlist link`;
+      const message = getCommand(mockMessage);
+      expect(message).toEqual({ args: 'link', command: 'playlist' });
+    });
+  });
+
+  describe('ChatInputCommandInteraction', () => {
+    test('should return a message with args and command', () => {
+      const mockMessage = mockDiscordChatInputInteraction();
+
+      const commandName = faker.random.word();
+      const commandValue = faker.random.word();
+
+      mockMessage.commandId = '1205239436145729638';
+      mockMessage.commandName = commandName;
+      mockMessage.options = {
+        _hoistedOptions: [
+          {
+            name: 'link',
+            type: 3,
+            value: commandValue
+          }
+        ]
+      } as any;
+      const message = getCommand(mockMessage);
+      expect(message).toEqual({ args: commandValue, command: commandName });
+    });
   });
 });
